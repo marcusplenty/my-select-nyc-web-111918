db1464070b16cef40e321270f3e78a00e3f62b5b
capable-publisher-7225 <marcusplenty@gmail.com>
2018-11-18 00:00:09 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+ i=0
+ arr2 = []
+ while i<array.length
+  if yield(array[i]) == TRUE
+    arr2 << yield(array[i])
+  end
+  i+=1
+ end
+ return arr2
 end
